0b6386710c5bb38099b5ddbf18603d528754b010
ValGan <valgan14@mail.ru>
2018-03-11 23:09:46 +0500

    V4 (Списки)

--- Snake_V/Program.cs
+++ Snake_V/Program.cs
@@ -11,20 +11,34 @@ namespace Snake_V
         static void Main(string[] args)
         {
             Point p1 = new Point(2,5,'^');
-            p1.draw();
-            Point p2 = new Point();
-            p2.x = 5;
-            p2.y = 9;
-            p2.sym = '&';
-            p2.draw();
-           // Draw(p2.x, p2.y, p2.sym);
-            /* int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
-            */
+            //p1.draw();
+            Point p2 = new Point(4,2,'$');
+            //p2.draw();
+            Point p3 = new Point(10, 15, '@');
+            Point p4 = new Point(22, 26, '!');
 
-            Console.ReadLine();
-        }
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+            
+            //numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2); 
+            pList.Add(p3);
+            pList.Add(p4);
+            for (int i = 0; i < pList.Count+1; i++)
+            {
+                pList[i].draw();
+            }
+
+			Console.ReadLine();
+		}
     }
 }
